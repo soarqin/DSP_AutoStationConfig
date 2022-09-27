b5d68c69bd949d43f5e15c71e2f53bf7e4a65eea
Soar Qin <soarchin@gmail.com>
2022-09-27 15:42:40 +0800

    Fix support for new DSP game version with new config entries
    * Fix compatibility with DSP v0.9.27
    * Add config entry for Advance Mining Machine Default Gathering Speed
    * Add config entry for Logistics Distributor Charging Power

--- AutoStationConfig/BuildingParametersPatch.cs
+++ AutoStationConfig/BuildingParametersPatch.cs
@@ -18,10 +18,41 @@ namespace Pasukaru.DSP.AutoStationConfig
         ){
             var entityPool = factory.entityPool;
             var stationId = entityPool[entityId].stationId;
-            if (stationId == 0) return;
-            
-            var component = factory.transport.stationPool[stationId];
-            component?.FixDuplicateWarperStores(stationId, factory);
+            if (stationId != 0)
+            {
+                var stationComponent = factory.transport.stationPool[stationId];
+                if (stationComponent is null)
+                    return;
+                if (stationComponent.isCollector) return;
+                /* Parameters[320] is power consumption, which is set to non-zero while copy-building */
+                if (parameters == null || parameters[320] == 0)
+                {
+                    stationComponent.SetStackCount();
+                    if (stationComponent.isVeinCollector)
+                    {
+                        stationComponent.SetGatheringSpeed(factory);
+                    }
+                }
+                if (!stationComponent.isVeinCollector)
+                {
+                    stationComponent.FixDuplicateWarperStores(stationId, factory);
+                }
+                return;
+            }
+
+            var dispenserId = entityPool[entityId].dispenserId;
+            if (dispenserId == 0) return;
+            var dispenserComponent = factory.transport.dispenserPool[dispenserId];
+            if (dispenserComponent is null)
+                return;
+            /* Don't deal with Dispensers with item filter set, which indicates a copy-build */
+            dispenserComponent.ApplyGuessFilter(factory);
+            if (filterId != 0) return;
+            /* Check if any parameter is not the default value, which indicates a copy-build */ 
+            if (parameters != null && (parameters[0] != 2 || parameters[1] != 0 || parameters[2] != 30000 || parameters[3] != 0)) return;
+            var itemProto = LDB.items.Select(factory.entityPool[entityId].protoId);
+            dispenserComponent.SetChargingPower(factory, itemProto.prefabDesc);
+            dispenserComponent.AddBotsFromInventory(itemProto.prefabDesc);
         }
     }
 
